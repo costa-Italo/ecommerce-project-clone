d409cda094d2b18f2ed9f55456f0e2e74582a14c
Italo Costa <italosiqueiradacosta@gmail.com>
2024-08-16 17:06:10 -0300

    🔹 feat: cursor-pointer na imagens da seção 'Hot Category'

--- src/components/HotCategory.tsx
+++ src/components/HotCategory.tsx
@@ -12,7 +12,7 @@
                     <img 
                         src={Img18.src} 
                         alt="" 
-                        className="grouw w-full max-md:mt-6 max-md:max-w-full"
+                        className="grouw w-full max-md:mt-6 max-md:max-w-full cursor-pointer"
                     />
                 </div>
                 <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
@@ -23,13 +23,13 @@
                                     <img 
                                         src={Img01.src} 
                                         alt="" 
-                                        className="w-full aspect-square max-md:mt-6"
+                                        className="w-full aspect-square max-md:mt-6 cursor-pointer"
                                     />
                                 </div>
                                 <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
                                     <div className="flex flex-col justify-center text-xs font-semibold text-white uppercase whitespace-nowrap max-md:mt-6">
                                         <div className="flex relative flex-col items-start pt-6 pb-20 w-full aspect-square max-md:pr-5">
-                                            <img src={Img03.src} alt="" className="object-cover absolute inset-0 size-full"
+                                            <img src={Img03.src} alt="" className="object-cover absolute inset-0 size-full cursor-pointer"
                                             />
                                                 <span className="relative px-3 py-2 mb-32 bg-gray-800 max-md:mb-10">
                                                 sale
@@ -42,10 +42,10 @@
                         <div className="mt-3 max-md:max-w-full">
                             <div className="flex gap-5 max-md:flex-col">
                                 <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
-                                    <img src={Img02.src} alt=""  className="w-full aspect-square max-md:mt-6"/>
+                                    <img src={Img02.src} alt=""  className="w-full aspect-square max-md:mt-6 cursor-pointer"/>
                                 </div>
                                 <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
-                                    <img src={Img20.src} alt="" className="w-full aspect-square max-md:mt-6"/>
+                                    <img src={Img20.src} alt="" className="w-full aspect-square max-md:mt-6 cursor-pointer"/>
                                 </div>
                             </div>
                         </div>
