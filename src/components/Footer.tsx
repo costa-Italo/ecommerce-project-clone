2b5dc6a612f52f2de11bfb23d4704ed7bbd7b8e6
Italo Costa <italosiqueiradacosta@gmail.com>
2024-08-17 09:58:04 -0300

    feat: cursor pointer e links de redes sociais no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,42 +10,50 @@ const Footer = () => {
             <div className='flex flex-row items-center justify-center w-full'>
                 <div className="w-96">
                     <img src={LogoCoral.src} alt="" />
-                    <p className="mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua</p>
+                    <p className="mt-5">Moda Autêntica para Todos os Momentos, Viva o Estilo Coral.</p>
                     <div className="flex flex-row mt-5 justify-start gap-10">
-                        <img src={Facebook.src} alt="" />
-                        <img src={Twitter.src} alt="" />
-                        <img src={Linkedin.src} alt="" />
-                        <img src={Instagram.src} alt="" />
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                        <img src={Facebook.src} alt="facebook" />
+                    </a>
+                    <a href="https://x.com" target="_blank" rel="noopener noreferrer">
+                        <img src={Twitter.src} alt="twitter" />
+                    </a>
+                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                        <img src={Linkedin.src} alt="Linkedin" />
+                    </a>
+                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">                     
+                        <img src={Instagram.src} alt="Instagram" />
+                    </a>                                               
                     </div>                   
                 </div>
                 <div className="w-96 ml-5">
                     <h1 className="font-bold" >CATALOG</h1>                  
                     <ul className="mt-5">
-                        <li className="text-gray-600">Necklaces</li>
-                        <li className="mt-2 text-gray-600">Hoodies</li>
-                        <li className="mt-2 text-gray-600">Jewerly Box</li>
-                        <li className="mt-2 text-gray-600">T-Shirt</li>
-                        <li className="mt-2 text-gray-600">Jacket</li>
+                        <li className="text-gray-600 cursor-pointer">Necklaces</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">Hoodies</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">Jewerly Box</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">T-Shirt</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">Jacket</li>
                     </ul>      
                 </div>
                 <div className="w-96">
                 <h1 className="font-bold" >ABOUT US</h1>                  
                     <ul>
-                        <li className="mt-2 text-gray-600">Our Producers</li>
-                        <li className="mt-2 text-gray-600">Sitemap</li>
-                        <li className="mt-2 text-gray-600">FAQ</li>
-                        <li className="mt-2 text-gray-600">About Us</li>
-                        <li className="mt-2 text-gray-600">Terms & Conditions</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">Our Producers</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">Sitemap</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">FAQ</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">About Us</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">Terms & Conditions</li>
                     </ul>      
                 </div>
                 <div className="w-96">
                 <h1 className="font-bold" >COSTUMER SERVICES</h1>                  
                     <ul>
-                        <li className="mt-2 text-gray-600">Contact Us</li>
-                        <li className="mt-2 text-gray-600">Track Your Order</li>
-                        <li className="mt-2 text-gray-600">Product CAre & Repair</li>
-                        <li className="mt-2 text-gray-600">Book An Appoiment</li>
-                        <li className="mt-2 text-gray-600">Shipping & Returns</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">Contact Us</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">Track Your Order</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">Product CAre & Repair</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">Book An Appoiment</li>
+                        <li className="mt-2 text-gray-600 cursor-pointer">Shipping & Returns</li>
                     </ul>      
                 </div>
             </div>      
