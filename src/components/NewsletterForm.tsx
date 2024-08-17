a091aca445388eb4f5cc8e76adc4cbe457ef945f
Italo Costa <italosiqueiradacosta@gmail.com>
2024-08-17 10:55:40 -0300

    feat: novo estilo do formulário e botão

--- src/components/NewsletterForm.tsx
+++ src/components/NewsletterForm.tsx
@@ -1,23 +1,17 @@
 const NewsletterForm = () => {
     return (
-        <div className='flex justify-between'>
-        <form className="flex mt-20 e max-md:flex-wrap justify-center w-full gap-48">
-            <div className="flex flex-col grow shrink-0  font-semibold basis-0 text-gray-800 text-opacity-50 w-fit ">
-                <label htmlFor="emailInput" className="sr-only">Email address</label>
-        <input
-            id="emailInput"
-            type="email"
-            placeholder="Email address..."
-            className="self-start ml-4 max-md:ml-2.5 bg-transparent border-none"
-            aria-label="Email address"
-        />
-            <div className="mt-4 h-0.5 bg-black border-2 border-black border-solid w-96" />
-        </div>
-        <button type="submit" className="">
-            <span className="px-4 bg-transparent">SUBMIT</span>
-            <div className="mt-4 h-0.5 bg-black border-2 border-black border-solid w-24" />
+        <div className='flex justify-between gap-10 items-center'>
+            <form className="flex overflow-hidden px-12 py-5 text-center rounded-xl border border-black border-solid font-[231] justify-center mt-2">
+                <input 
+                    type="text"
+                    id="storeName"
+                    placeholder="Cadastre seu email gratuitamente"
+                    className="bg-transparent outline-none w-96 text-center"
+                />           
+        </form>
+        <button className="px-7 py-5 text-white bg-gray-800 max-md:px-5 transition-transform transform hover:scale-105 w-44 h-14 flex items-center justify-center">
+                    Enviar
         </button>
-    </form>
     </div>
     )
 }
