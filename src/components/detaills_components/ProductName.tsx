74aa5f51662016a7b5d5c60b77a1b57d8e2302af
Italo Costa <italosiqueiradacosta@gmail.com>
2024-08-23 09:27:23 -0300

    feat: nome do produto adicionado.

---
+++ src/components/detaills_components/ProductName.tsx
@@ -0,0 +1,11 @@
+    import React from "react";
+
+    const ProductName = () => {
+    return (
+        <div className="ml-10">
+        <h1 className="font-bold text-4xl">Basic hooded sweatshirt in pink</h1>
+        </div>
+    );
+    };
+
+    export default ProductName;
--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+    import ProductName from "@/components/detaills_components/ProductName";
+    import React from "react";
 
-const details = () => {
+    const details = () => {
     return (
-        <div className='flex flex-col items-center justify-center'>
-            <h1>Componente aqui</h1>
+        <div className="flex flex-col items-center justify-center">
+        <div className="flex w-full mt-10">
+            <ProductName />
         </div>
-    )
-}
+        </div>
+    );
+    };
 
-export default details
+    export default details;
