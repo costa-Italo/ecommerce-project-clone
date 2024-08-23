426541f91fe7e496d6b2edfd261f1562c4d84a6b
Italo Costa <italosiqueiradacosta@gmail.com>
2024-08-23 09:19:02 -0300

    feat: página de produtos criada

---
+++ src/pages/details.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const details = () => {
+    return (
+        <div className='flex flex-col items-center justify-center'>
+            <h1>Componente aqui</h1>
+        </div>
+    )
+}
+
+export default details
