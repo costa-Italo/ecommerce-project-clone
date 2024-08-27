cc5037313f7a45b0c62eef7561ec9c2a14feaef1
Italo Costa <italosiqueiradacosta@gmail.com>
2024-08-27 08:49:08 -0300

    feat: novo layout dos detalhes dos produtos

---
+++ src/pages/product-page.tsx
@@ -0,0 +1,15 @@
+    const ProductPage = () => {
+    return (
+        <div className="flex overflow-hidden flex-col bg-white">
+            <p>Header</p>
+            <p>Detalhe dos produtos</p>
+            <div className="flex gap-6 items-center self-end mt-16 mr-28 max-md:mt-10 max-md:mr-2.5">
+                <p>BANDEIRA DOS CARTÕES</p>
+            </div>
+            <p>Formulário de isncrição</p>
+            <p>Footer</p>
+        </div>
+    );
+    };
+
+    export default ProductPage;
